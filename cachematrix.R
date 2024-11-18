c1af6d9161c4c59a8f685b56c3502fa5bf5b0bb7
king5ka <143033808+king5ka@users.noreply.github.com>
2024-11-18 15:53:43 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of this exercise is to create a function that will store/cache a matrix inverse so I don't have to go through the process multiple times
+## because calculating the inverse of a matrix consumes time and computing energy
 
-## Write a short comment describing this function
+## this function is going to make a matrix that lets us cache the inverse easier
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+  set <- function(y) {
+          x <<- y
+          i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
+## I am  using <<- because I don't necessarily have those values in our environment yet, and so that R knows it's not in m current environment
 
 
-## Write a short comment describing this function
+## this function will compute the inverse of the matrix I specified above, or collect it from the cache I created and notify you about it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+          message("retrieving cached inverse")
+          return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
 }
+
+## tada!!! I tested a matrix here:
+M<- matrix(c(2,4,6,8),2,2)
+MC<- makeCacheMatrix(M)
+cacheSolve(MC)
+## if I call cacheSolve(MC) again, it will give the 'retrieving cached inverse' message because I just calculated it on line 40
